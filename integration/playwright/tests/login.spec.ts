5647485fd71b57c0f743b1e07ad3453b66c169a1
nimya-aot <nimya.john@aot-technologies.com>
2026-03-25 08:42:46 -0700

    Removed env variable

--- integration/playwright/tests/login.spec.ts
+++ integration/playwright/tests/login.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("Visits the login page to validate the URL", async ({ page }) => {
-  const depEnv = process.env.DEP_ENV;
-
   await page.goto("/");
   //Login button visibility
   await expect(page.locator('[data-testid="login-button"]')).toBeVisible();
